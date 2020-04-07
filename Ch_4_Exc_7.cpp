7772895b86d8111871ac4f026a8de6bd14cd96bd
Tigran <tigran.ghazaryan@outlook.com>
2020-04-07 21:19:27 +0400

    Update Ch_4_Exc_7.cpp

--- Ch_4_Exc_7.cpp
+++ Ch_4_Exc_7.cpp
@@ -1,14 +1,21 @@
 #include "C:\Users\PC\Documents\Visual Studio 2019\Header.h"
 //Excercise 7
+// Makes a smiple calculator, that inputs digits or written numbers (from list of 0-9) (converts it)
+// And prints out the answer in a specific form
 
 int main()
 {
 	string choice;
+	// First and second input of numbers as digits
 	double first = 0;
 	double second = 0;
 	char operation;
+	// First and second input of numbers in written form
 	string number;
 	string number_2;
+	// Creates a list of numbers in written form
+	// This will help us identify the specific Digit according to its position in the list
+	// (e.g. "zero" is positioned at [0]
 	vector <string> numbers(10);
 	numbers[0] = "zero";
 	numbers[1] = "one";
@@ -21,6 +28,7 @@ int main()
 	numbers[8] = "eight";
 	numbers[9] = "nine";
 
+	//These would be our indicators for checking the above list for the two String inputs through a loop
 	double i = 0;
 	double j = 0;
 	
@@ -28,12 +36,15 @@ int main()
 		<< "Do you want 'written' or 'integer' imputs for the numbers?\n";
 	while (cin >> choice)
 	{
+		//Asks if the user wants an Integer version of the calculator
 		if (choice == "integer")
 		{
 			while (cin >> first >> operation >> second)
 			{
+				//These assignments are done for the case of divisibility (modulo), since doubles cannot do this operation
 				int first_1 = first;
 				int second_1 = second;
+				//Using a swtich operation instead of "If" makes it simpler in case of Char inputs
 				switch (operation)
 				{
 				case '+':
@@ -46,6 +57,7 @@ int main()
 					cout << "The product of " << first << " and " << second << " is " << first * second << endl;
 					break;
 				case '/':
+					// Cover cases where you might divide by 0 (so there is no error)
 					if (second == 0)
 						cout << "Invalid Operation: can't divide by 0\n";
 					else
@@ -63,21 +75,26 @@ int main()
 				};
 			};
 		}
+		//Asks if the user wants the Written version of the calculator
 		else if (choice == "written")
 		{
+			//Written form comes with limitations to single-digits
 			cout << "Choose the numbers from 0 - 9 for the written version\n";
 			while (cin >> number >> operation >> number_2)
 			{
+				//The following two loops both for i and j do the following:
+				//They go through the list and compare the given written input "number" to the list of the vectors
+				// E.g. the user inputs "five" and we compare it to the list numbers[4] = "five"
+				
 				while (i != 9 && number != numbers[i])
 					++i;
-				while (i != 0 && number != numbers[i])
-					--i;
 				while (j != 9 && number_2 != numbers[j])
 					++j;
-				while (j != 0 && number_2 != numbers[j])
-					--j;
+				
+				// if the number is found we do the operation similar to the above one
 				if (number == numbers[i] && number_2 == numbers[j])
 				{
+					
 					int i_1 = i;
 					int j_1 = j;
 					switch (operation)
@@ -107,9 +124,18 @@ int main()
 						cout << "ERROR, unrecognized operation\n";
 						break;
 					};
+					// We prepare for the next loop
+					// (So that we start from 0 to ++ till 9 again)
+					i = 0;
+					j = 0;
 				}
+				// If the (written form of the) number isn't found in our list, we let the user know
 				else
+				{
 					cout << "ERROR. Unrecognized number, or a number that is not from 0 - 9.\n";
+					//Again we prepare for the next loop
+					i = 0;
+					j = 0;
 			};
 		}
 		else
